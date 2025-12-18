24235f10cd14d11e965309906bc0d16e3ca60948
sriram6946 <sriram6946@gmail.com>
2025-12-18 14:31:20 +0530

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+const input = document.getElementById("input")
+
+function reverseString(str){
+    return str.split("").reverse().join("")
+}
+
+function check(){
+    const value = input.value;
+    const reverse = reverseString(value);
+
+    if(value === reverse){
+        alert("It is a palindrome")
+    } else {
+        alert("It is not a palindrome")
+    }
+}
